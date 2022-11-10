cbf3ee10be47e55fcf61c0a88b097e1581caca67
caio-galvao <caio.r.galvao@gmail.com>
2022-11-10 10:55:26 -0300

    Adiciona mais 2 formas de saída para test_instancias_reais.py

--- test_instancias_reais.py
+++ test_instancias_reais.py
@@ -2,26 +2,47 @@ import csv
 import pandas as pd
 from new_aws_model import otimizaModelo
 
-def preProcessamento(instancia):
+def preProcessamento(instance):
     totalDemand = pd.read_csv('TOTAL_demand.csv')
-    return totalDemand[instancia].values.tolist()
+    return totalDemand[instance].values.tolist()
 
-def output(instancia, cost, values):
+def output(instance, cost, values):
     output = open('resultCost.csv', 'w')
     writer = csv.writer(output)
     writer.writerow(['instancia', 'total_cost'])
-    writer.writerow([instancia, cost])
+    writer.writerow([instance, cost])
     
-    values.insert(0, instancia)
+    values.insert(0, instance)
 
     output = open('resultValues.csv', 'w')
     writer = csv.writer(output)
     writer.writerow(values)
+    createTotalPurchases(instance, values)
+
+def createTotalPurchases(instance, values):
+    values.pop(0)
+    valuesDivided = list(dividirLista(values, 4))
+
+    output = open('total_purchases.csv', 'w')
+    writer = csv.writer(output)
+    writer.writerow(['instanceType', 'market', 'count_active', 'count_reserves'])
+    
+    # ValueSet = [a_od, r_od, a_re, r_re] for each 
+
+    for valueSet in valuesDivided:
+        writer.writerow([instance, 'OnDemand', int(valueSet[0]), int(valueSet[1])])
+    
+    for valueSet in valuesDivided:
+        writer.writerow([instance, 'Reserved', int(valueSet[2]), int(valueSet[3])])
+
+def dividirLista(lista, n):
+    for i in range(0, len(lista), n):
+        yield lista[i:i + n]
 
 #Teste com instancias reais
-instancia = 'c4.2xlarge' #change the instance here
+instance = 'c4.2xlarge' #change the instance here
 
-demand = preProcessamento(instancia)
+demand = preProcessamento(instance)
 
 t = len(demand)
 
@@ -36,4 +57,9 @@ y = 8760 #reserve duration (1 year)
 result = otimizaModelo(t, demand, p_od, p_re, u, y)
 cost = result[0]
 values = result[1]
-output(instancia, cost, values)
+output(instance, cost, values)
+
+info = open('info.txt', 'w')
+info.writelines(["Instance: " + instance, "\nOn demand price: " + str(p_od), "\nReserve hourly price: " + str(p_re),
+                "\nReserve upfront price: " + str(u), "\nReserve duration: " + str(y), "\nTotal cost: " + str(cost)]) 
+info.close()
