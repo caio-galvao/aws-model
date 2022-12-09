8adbeed071ba070acb25d4c209af1b88d68a0b13
caio-galvao <caio.r.galvao@gmail.com>
2022-12-09 17:40:05 -0300

    Melhora a saída da versão 4

--- version4/build-simulation.py
+++ version4/build-simulation.py
@@ -2,23 +2,35 @@ import csv
 import pandas as pd
 from new_aws_model import otimizaModelo
 
-def createTotalPurchases(instance, values, market_names):
-    valuesDivided = list(dividirLista(values, 2 * len(market_names)))
+def createTotalPurchases(raw_values, t, instance_names, market_names):
+    values = tranformarEmLista(raw_values, t, len(instance_names), len(market_names))
 
-    output = open('total_purchases_' + instance + '.csv', 'w')
-    writer = csv.writer(output)
-    writer.writerow(['instanceType', 'market', 'count_active', 'count_reserves'])
-    
-    # ValueSet = [a_od, r_od, a_re, r_re] for each 
+    for i_instancia in range(len(instance_names)):
+        output = open('total_purchases_' + instance_names[i_instancia] + '.csv', 'w')
+        writer = csv.writer(output)
+        writer.writerow(['instanceType', 'market', 'count_active', 'count_reserves'])
+        
+        for i_mercado in range(len(market_names)):
+            for i_tempo in range(t):
+                activ = values[i_tempo][i_instancia][i_mercado][0]
+                reserves = values[i_tempo][i_instancia][i_mercado][1]
+                writer.writerow([instance_names[i_instancia], market_names[i_mercado], activ, reserves])
+        
+        output.close()
 
-    for i in range(len(market_names)):
-        market = market_names[i]
-        for valueSet in valuesDivided:
-            writer.writerow([instance, market, int(valueSet[2*i]), int(valueSet[2*i + 1])])
-
-def dividirLista(lista, n):
-    for i in range(0, len(lista), n):
-        yield lista[i:i + n]
+def tranformarEmLista(values, t, num_instances, num_markets):
+    index = 0
+    lista = []
+    for i_tempo in range(t):
+        lista_tempo = []
+        for i_instancia in range(num_instances):
+            lista_instancia = []
+            for i_mercado in range(num_markets):
+                lista_instancia.append([values[index], values[index+1]])
+                index += 2
+            lista_tempo.append(lista_instancia)
+        lista.append(lista_tempo)
+    return lista
 
 raw_input = pd.read_csv('input.csv')
 instances = raw_input['instance'].value_counts()
@@ -31,16 +43,18 @@ writerCost.writerow(['total_cost'])
 
 input_data = []
 total_demand = []
+instance_names = []
 
 for instance in instances.index:
+    market_names = []
     instance_input = []
-    #market_names = []
+    instance_names.append(instance)
 
     for i in range(len(raw_input)):
         line = raw_input.iloc[i]
         if line['instance'] == instance:
             instance_input.append([line['p_hr'],line['p_up'], line['y']])
-            #market_names.append(line['market_name'])
+            market_names.append(line['market_name'])
     input_data.append(instance_input)
 
     instance_demand = raw_demand[instance].values.tolist()
@@ -52,6 +66,6 @@ t = len(total_demand[0])
 
 result = otimizaModelo(t, total_demand, input_data)
 cost = result[0]
-values = result[1]
+createTotalPurchases(result[1], t, instance_names, market_names)
 
 writerCost.writerow([cost])
